5ca9d26bb4ffc55b1e8e40f2b9c13993b8431c43
Queen Tijani <tijaniqueen17@gmail.com>
2020-09-30 17:58:25 +0100

    Added movie search app 🎉

---
+++ Personal Projects/react-movie-search/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import SearchMovies from "./components/SearchMovies";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Personal Projects/react-movie-search/src/components/SearchMovies.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import MovieCard from "./MovieCard";
+
+function SearchMovies(props) {
+  const [query, setQuery] = useState("");
+
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=ba953dd1d453af6ce1cf60bd2dc9f416&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.error(err);
+    }
+
+    setQuery("");
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Enola Holmes"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
+}
+
+export default SearchMovies;
