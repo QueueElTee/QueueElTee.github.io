444abc71a2cda14d296f058c05f626c5ebb42dc8
Queen Tijani <tijaniqueen17@gmail.com>
2022-04-25 18:40:48 +0100

    Made Nav and Hero Sections Responsive

--- Frontend Mentor/Loopstudios Landing Page/script.js
+++ Frontend Mentor/Loopstudios Landing Page/script.js
@@ -1,8 +1,8 @@
 function navFunction(){
-    var x = document.getElementById("nav");
-    if(x.className === "nav"){
+    var x = document.getElementById("navControl");
+    if(x.className === "navControl"){
         x.className += " responsive";
     } else{
-        x.className = "nav";
+        x.className = "navControl";
     }
 }
