832bec55c4a24ab0138aecf10dd06678915bdde0
Queen Tijani <tijaniqueen17@gmail.com>
2022-05-01 16:09:45 +0100

    Fix Hamburger Overlay Issues

--- Frontend Mentor/Loopstudios Landing Page/script.js
+++ Frontend Mentor/Loopstudios Landing Page/script.js
@@ -2,7 +2,9 @@ function navFunction(){
     var x = document.getElementById("navControl");
     if(x.className === "navControl"){
         x.className += " responsive";
+        document.getElementsByTagName("body")[0].style.overflow = "hidden";
     } else{
         x.className = "navControl";
+        document.getElementsByTagName("body")[0].style.overflow = "scroll";
     }
 }
